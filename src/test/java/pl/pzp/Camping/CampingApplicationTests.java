590691dce61745b5519b7471a324a49654ad2b1b
XantisS0 <patryk.makowski23@gmail.com>
2020-12-18 14:54:48 +0100

    Add init commit

---
+++ src/test/java/pl/pzp/Camping/CampingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pzp.Camping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
